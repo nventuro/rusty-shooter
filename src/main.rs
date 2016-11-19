16c37352ea872c411b3931f1600f04bfafc1609a
Nicolás Venturo <nicolas.venturo@gmail.com>
2016-11-19 01:57:41 -0300

    Moved logic to phi

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,8 @@ extern crate sdl2;
 mod phi;
 mod views;
 
-use phi::{Events, Phi, View, ViewAction};
-
 fn main() {
-    // Initialize SDL2
-    let sdl_context = sdl2::init().unwrap();
-    let video = sdl_context.video().unwrap();
-    let mut timer = sdl_context.timer().unwrap();
-
-    // Create the window
-    let window = video.window("rusty-shooter", 800, 600)
-        .position_centered().opengl()
-        .build().unwrap();
-
-    let mut context = Phi {
-        renderer: window.renderer().accelerated().build().unwrap(),
-        events: Events::new(sdl_context.event_pump().unwrap())
-    };
-
-    let mut current_view: Box<View> = Box::new(::views::RedView);
-
-    let interval = 1_000 / 60;
-    let mut before = timer.ticks();
-    let mut last_second = timer.ticks();
-    let mut fps = 0;
-
-    loop {
-        let now = timer.ticks();
-        let dt = now - before;
-        let elapsed = dt as f64 / 1_000.0;
-
-        if dt < interval {
-            timer.delay(interval - dt);
-            continue;
-        }
-
-        before = now;
-        fps += 1;
-
-        if now - last_second > 1_000 {
-            println!("FPS: {}", fps);
-            last_second = now;
-            fps = 0;
-        }
-
-        context.events.pump();
-
-        match current_view.render(&mut context, elapsed) {
-            ViewAction::None => context.renderer.present(),
-            ViewAction::ChangeView { new_view } => current_view = new_view,
-            ViewAction::Quit => break
-        }
-    }
+    ::phi::spawn("rusty-shooter", |_| {
+        Box::new(::views::RedView)
+    });
 }
--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -6,6 +6,8 @@ use sdl2::render::Renderer;
 struct_events! {
     keyboard: {
         key_escape: Escape,
+        key_up: Up,
+        key_down: Down,
         key_space: Space
     },
     other: {
@@ -30,4 +32,71 @@ pub trait View {
     ///
     /// `elapsed` is expressed in seconds.
     fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
+
+pub fn spawn<F>(title: &str, init: F)
+where F: Fn(&mut Phi) -> Box<View> {
+    // Initialize SDL2
+    let sdl_context = ::sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    let mut timer = sdl_context.timer().unwrap();
+
+    // Create the window
+    let window = video.window(title, 800, 600)
+        .position_centered().opengl()
+        .build().unwrap();
+
+    // Create the context
+    let mut context = Phi {
+        events: Events::new(sdl_context.event_pump().unwrap()),
+        renderer: window.renderer()
+            .accelerated()
+            .build().unwrap(),
+    };
+
+    // Create the initial view
+    let mut current_view = init(&mut context);
+
+    // Frame timing
+    let interval = 1_000 / 60;
+    let mut before = timer.ticks();
+    let mut last_second = timer.ticks();
+    let mut fps = 0;
+
+    loop {
+        let now = timer.ticks();
+        let dt = now - before;
+        let elapsed = dt as f64 / 1_000.0;
+
+        // Lock to the intended framerate
+        if dt < interval {
+            timer.delay(interval - dt);
+            continue;
+        }
+
+        before = now;
+        fps += 1;
+
+        if now - last_second > 1_000 {
+            println!("FPS: {}", fps);
+            last_second = now;
+            fps = 0;
+        }
+
+
+        // Logic & rendering
+
+        context.events.pump();
+
+        match current_view.render(&mut context, elapsed) {
+            ViewAction::None =>
+                context.renderer.present(),
+
+            ViewAction::ChangeView { new_view } =>
+                current_view = new_view,
+
+            ViewAction::Quit =>
+                break
+        }
+    }
 }
